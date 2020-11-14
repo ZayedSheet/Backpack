a917d8f531c66479cea4cc6af19be8e12da2c4ed
root <root@DESKTOP-C0AAEJQ.localdomain>
2020-11-14 17:32:53 -0500

    Context api

--- backpack/src/App.js
+++ backpack/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [calendar, setCalendar] = useState();
+  const Context = React.createContext();
+  const store = {
+    setCalendar : () => {},
+    calendar : [],
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={store}>
+      {/* routes */}
+      hello
+    </Context.Provider>
   );
 }
 
